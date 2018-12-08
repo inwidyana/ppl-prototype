2c0c27ace85e8d171d1c605a95c0967a5224c541
aflahnadhif <aflahnadhif@gmail.com>
2018-12-08 21:27:19 +0700

    latest commit

--- resources/views/spending/index.blade.php
+++ resources/views/spending/index.blade.php
@@ -1,44 +1,3 @@
-{{--<table>--}}
-    {{--<thead>--}}
-        {{--<tr>--}}
-            {{--<td>Category</td>--}}
-            {{--<td>Budget</td>--}}
-            {{--<td>Spending</td>--}}
-        {{--</tr>--}}
-    {{--</thead>--}}
-    {{--<tbody>--}}
-        {{--<tr>--}}
-            {{--<td>Make Up</td>--}}
-            {{--<td>1,500,000</td>--}}
-            {{--<td>0</td>--}}
-        {{--</tr>--}}
-        {{--<tr>--}}
-            {{--<td>Gas</td>--}}
-            {{--<td>250,000</td>--}}
-            {{--<td>0</td>--}}
-        {{--</tr>--}}
-        {{--<tr>--}}
-            {{--<td>Shopping</td>--}}
-            {{--<td>500,000</td>--}}
-            {{--<td>0</td>--}}
-        {{--</tr>--}}
-        {{--<tr>--}}
-            {{--<td>Food</td>--}}
-            {{--<td>900,000</td>--}}
-            {{--<td>0</td>--}}
-        {{--</tr>--}}
-        {{--<tr>--}}
-            {{--<td>Groceries</td>--}}
-            {{--<td>900,000</td>--}}
-            {{--<td>0</td>--}}
-        {{--</tr>--}}
-        {{--<tr>--}}
-            {{--<td>Phone</td>--}}
-            {{--<td>160,000</td>--}}
-            {{--<td>0</td>--}}
-        {{--</tr>--}}
-    {{--</tbody>--}}
-{{--</table>--}}
 <!doctype html>
 <html lang="en">
 <head>
@@ -77,7 +36,47 @@
 <div class="container">
     <div class="billboard">
         <h1>Spending</h1>
-        <div id="example-table"></div>
+        <table class="table">
+        <thead>
+        <tr>
+        <td>Category</td>
+        <td>Budget</td>
+        <td>Spending</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+        <td>Make Up</td>
+        <td>1,500,000</td>
+        <td>0</td>
+        </tr>
+        <tr>
+        <td>Gas</td>
+        <td>250,000</td>
+        <td>0</td>
+        </tr>
+        <tr>
+        <td>Shopping</td>
+        <td>500,000</td>
+        <td>0</td>
+        </tr>
+        <tr>
+        <td>Food</td>
+        <td>900,000</td>
+        <td>0</td>
+        </tr>
+        <tr>
+        <td>Groceries</td>
+        <td>900,000</td>
+        <td>0</td>
+        </tr>
+        <tr>
+        <td>Phone</td>
+        <td>160,000</td>
+        <td>0</td>
+        </tr>
+        </tbody>
+        </table>
     </div>
 </div>
 
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,6 +15,23 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/');
+
+        $response->assertViewIs('welcome');
+    }
+
+    public function testView()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/profile');
+
+        $response->assertViewIs('myprofile');
+    }
+
+    public function testAuth()
+    {
+        $response = $this->post('register', ['name' => 'jojon', 'email' => 'bebek', 'password' => 'testing', 'password-confirm' => 'test']);
+
+        $response->assertSessionHasErrors(['password']);
     }
 }
